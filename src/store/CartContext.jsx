4f317be80ee3ce18c0f096909f4570c086910308
bofrank <bo@bofrank.com>
2026-04-10 20:37:42 +0900

    starting cart context provider

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+    item: [],
+    addItem: (item) => {},
+    removeItem: (id) => {},
+});
+
+function cartReducer(state, action){
+    if(action.type === 'ADD_ITEM') {
+        const existingCartItemIndex = state.items.findImdex(
+            (item) => item.id === action.item.id
+        );
+
+        const updatedItems = [...state.items];
+
+        if(existingCartItemIndex > -1) {
+            const existingItem = state.items[existingCartItemIndex]
+            const updatedItem = {
+              ...existingItem,
+              quantity: existingItem.quantity + 1
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems.push({...action.item, quantity: 1 });
+        }
+
+        return { ...state, items: updatedItems };
+
+    }
+
+    if(action.type === 'REMOVE_ITEM') {
+        // ... remove the stae for a meal item
+    }
+
+    return state;
+
+}
+
+export function CartContextProvider({children}){
+    useReducer(cartReducer, { items: [] });
+    
+    return <CartContext>{children}</CartContext>;
+}
+
+export default CartContext;
