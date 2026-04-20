2a3949ceb338cf1c5187ac4ce234f8d51d9eb9d5
bofrank <bo@bofrank.com>
2026-04-20 13:32:53 +0900

    handling sync errors and custom hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,27 +1,24 @@
-import { useEffect, useState } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../Hooks/useHttp";
+
+const requestConfig = {};
 
 export default function Meals(){
-    const [loadedMeals, setLoadedMeals] = useState([]);
+    const { 
+        data: loadedMeals, 
+        isLoading, 
+        error, 
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
+    if(isLoading){
+        return <p>Fetching meals...</p>;
+    }
 
-            if(!response.ok){
-                return;
-            }
-
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-
-        fetchMeals();
-    }, []);
-
-    return (<ul id="meals">
-        {loadedMeals.map((meal) => (
-            <MealItem key={meal.id} meal={meal} />
-        ))}</ul>
+    return (
+        <ul id="meals">
+            {loadedMeals.map((meal) => (
+                <MealItem key={meal.id} meal={meal} />
+            ))}
+        </ul>
     );
 }
